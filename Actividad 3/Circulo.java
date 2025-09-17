83de5bafbc664242f42850d6b7f8c855ee5f5055
LaK <134339275+David-Narvaez@users.noreply.github.com>
2025-09-17 16:52:33 -0500

    hola

---
+++ Actividad 3/Circulo.java
@@ -0,0 +1,32 @@
+package paqueteCirculo;
+
+public class Circulo {
+	
+	private double radio = 1.0;
+	
+	public Circulo() {
+		radio=1.0;
+	}
+	public Circulo (double radio) {
+		this.radio=radio;
+	}
+	public double getRadio() {
+		return radio;
+	}
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	public double getArea() {
+		double area=Math.PI*Math.pow(radio, 2);
+		return area;
+	}
+	public double getPerimetro() {
+		double perimetro= 2*Math.PI*radio;
+	}
+	@Override
+	public String toString() {
+		return "Circulo [radio del circulo=" + radio + ", con area =" + getArea() 
+		+ ", y con perimetro =" + getPerimetro() + "]";
+	}
+}
+	
